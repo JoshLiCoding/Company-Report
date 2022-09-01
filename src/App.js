71eba3b0d4f2c0b2b3c691d35b723e3f255f7a6a
Joshua Li <joshual2004.work@gmail.com>
2022-09-01 16:49:38 -0400

    changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import{BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Home from './Home/Home.js'
 import Google from './Google/Google.js'
 import Page404 from './404/404.js'
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
+const App = () => (
+  <div className='App'>
+    <HashRouter>
       <Switch>
-      <Route exact path="/Company-Report">
+        <Route exact path="/">
           <Home />
         </Route>
-      <Route path="/Company-Report/google">
+        <Route exact path="/google">
           <Google />
         </Route>
-      <Route path="/">
+        <Route path="/">
           <Page404 />
         </Route>
       </Switch>
-      </Router>
-      
+    </HashRouter>
     </div>
-  );
-}
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <HashRouter>
+      <App />
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
